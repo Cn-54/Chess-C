4b3a1520b7371b61a2324aec6e2f0e75f7433110
cn-54 <238699711+Cn-54@users.noreply.github.com>
2026-09-11 19:02:24 +0100

    added thing to make actual random options

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include "game.h"
 #include "uci.h"
+#include <stdlib.h>
+#include <time.h>
 
-int main(void)
-{
+
+int main(void){
+    srand(time(NULL));
     Game *game = Create_Game();
 
     init_board(game);
